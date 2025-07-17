bb1aa6fc17429ca4c4df10bf3c3b7ff9e0c3d041
cjriley20 <20461606+cjriley20@users.noreply.github.com>
2025-07-17 12:43:53 -0400

    Align main header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -12,7 +12,7 @@ const Header = () => {
     <header>
       <SuperHeader />
       <MainHeader>
-        <Logo />
+        <HeaderLogo />
         <Nav>
           <NavLink href="/sale">Sale</NavLink>
           <NavLink href="/new">New&nbsp;Releases</NavLink>
@@ -27,11 +27,25 @@ const Header = () => {
 };
 
 const MainHeader = styled.div`
-  padding: 0 32px;
+  padding: 20px 32px;
   border-bottom: 1px solid ${COLORS.gray[300]};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
 `;
 
-const Nav = styled.nav``;
+const HeaderLogo = styled(Logo)`
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+  left: 32px;
+`;
+
+const Nav = styled.nav`
+  display: flex;
+  gap: 64px;
+`;
 
 const NavLink = styled.a`
   font-size: 1.125rem;
